35d9d4e12bb61cd14377cf0bf7d124b5020805a9
Kevin Condezo <86019347+KevinC2V@users.noreply.github.com>
2024-02-20 00:32:48 -0500

    update sequential version

--- src/LogisticRegression/SequentialLogisticRegression.java
+++ src/LogisticRegression/SequentialLogisticRegression.java
@@ -2,52 +2,22 @@ package LogisticRegression;
 
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
+import java.util.Arrays;
 import java.util.Locale;
 
 public class SequentialLogisticRegression {
-    /**
-     * Weights to learn
-     */
+
     protected double[] weights;
-
-    /**
-     * Learning rate
-     */
+    protected int numFeatures;
     protected double learningRate;
+    protected int numIterations;
+    protected double threshold; // Threshold to determine the class (between 0 and 1)
 
-    /**
-     * Maximum number of iterations
-     */
-    protected int maxIterations;
-
-    /**
-     * Minimum change of the weights (STOP criteria)
-     */
-    protected double minDelta;
-
-    /**
-     * Classification cut off
-     */
-    protected double cutOff;
-
-    // TODO: Revisar
-//    public LogisticRegression.ParallelLogisticRegression(int n) {
-//        weights = new double[n];
-//        this.learningRate = 0.0001;
-//        this.maxIterations = 3000;
-//        this.minDelta = 0.0001;
-//        this.cutOff = 0.5;
-//    }
-
-    /**
-     * Constructor
-     */
-    public SequentialLogisticRegression(int n, double learningRate, int maxIterations, double minDelta, double cutOff) {
-        weights = new double[n];
+    public SequentialLogisticRegression(int numFeatures, double learningRate, int numIterations, double threshold) {
+        this.numFeatures = numFeatures;
         this.learningRate = learningRate;
-        this.maxIterations = maxIterations;
-        this.minDelta = minDelta;
-        this.cutOff = cutOff;
+        this.numIterations = numIterations;
+        this.threshold = threshold;
     }
 
     /**
@@ -58,154 +28,96 @@ public class SequentialLogisticRegression {
     }
 
     /**
-     * Training function
-     * Use Stochastic Gradient Descent
+     * Training using Batch Gradient Descent
      */
-    public void trainModelWithSGD(double[][] X, int[] Y) {
-        double likelihood = 0.0;
-        double[] weightsPrev = new double[weights.length];
-        double maxWeightDev;
-
+    public void trainModelWithBGD(double[][] X, int[] Y) {
+        weights = new double[numFeatures]; // filled with zeros
         // Iterate until maxIterations or the STOP condition is met
-        for (int n = 0; n < maxIterations; n++) {
-            likelihood = 0.0;
+        for (int n = 0; n < numIterations; n++) {
+            double[] gradient = new double[numFeatures];
 
-            // Store previous weights
-            System.arraycopy(weights, 0, weightsPrev, 0, weights.length);
-
-            // Update weights
+            // Compute gradient for each feature
             for (int i = 0; i < X.length; i++) {
                 double yPredicted = computePrediction(X[i]);
+                double error = yPredicted - Y[i];
 
-                for (int j = 0; j < weights.length; j++) {
-                    weights[j] = weights[j] + learningRate * (Y[i] - yPredicted) * X[i][j];
-                }
-                // Compute log-likelihood
-                likelihood += Y[i] * Math.log(computePrediction(X[i])) + (1 - Y[i]) * Math.log(1 - computePrediction(X[i]));
-
-            }
-            if (n % 5000 == 0)
-                System.out.println("Iteration " + n + ": log likelihood = " + likelihood);
-
-            // Check STOP criteria
-            maxWeightDev = 0.0;
-            for (int j = 0; j < weights.length; j++) {
-                if ((Math.abs(weights[j] - weightsPrev[j]) / (Math.abs(weightsPrev[j]) + 0.01 * minDelta)) > maxWeightDev) {
-                    maxWeightDev = (Math.abs(weights[j] - weightsPrev[j]) / (Math.abs(weightsPrev[j]) + 0.01 * minDelta));
+                for (int j = 0; j < numFeatures; j++) {
+                    gradient[j] += error * X[i][j];
                 }
             }
-            if (maxWeightDev < minDelta) {
-                System.out.println("STOP criteria met: Iteration " + n + ", Log-likelihood = " + likelihood);
-                break;
+
+            // Update weights using the gradient
+            for (int j = 0; j < numFeatures; j++) {
+                weights[j] -= learningRate * gradient[j] / X.length;
             }
+
+            if ((n+1) % 5000 == 0)
+                System.out.println("Iteration " + (n+1) + ": gradient = " + Arrays.toString(gradient));
         }
-        System.out.println("Final log-likelihood= " + likelihood);
-        System.out.println();
     }
 
-    /**
-     * Use the model to compute prediction for the given x
-     */
     private double computePrediction(double[] x) {
-        double logit = 0.0;
-        for (int i = 0; i < weights.length; i++) {
-            logit += weights[i] * x[i];
+        double z = 0.0;
+        for (int i = 0; i < numFeatures; i++) {
+            z += weights[i] * x[i];
         }
-        return sigmoid(logit);
+        return sigmoid(z);
     }
 
-    /**
-     * Print the model
-     */
-    public void printModel() {
-        DecimalFormat df = new DecimalFormat("###.###");
-        df.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.US));
-        System.out.println("Logistic regression weights:");
-        for (double weight : weights) {
-            System.out.print(df.format(weight) + " ");
-        }
-        System.out.println();
-    }
-
-    /**
-     * Score data with the model
-     */
     public double[] scoreData(double[][] data) {
         int n = data.length;
         double[] predictedY = new double[n];
-
         for (int i = 0; i < n; i++) {
             predictedY[i] = computePrediction(data[i]);
         }
         return predictedY;
     }
 
-    /**
-     * Compute error rates
-     */
-    public void computeErrors(int[] Y, double[] predictedY) {
+    public void evaluateModel(int[] Y, double[] predictedY) {
         int FP = 0;
         int FN = 0;
         int TP = 0;
         int TN = 0;
-        double FNR;
-        double FPR;
+        double precision;
+        double recall;
+        double accuracy;
 
         for (int i = 0; i < predictedY.length; i++) {
-            int predY = ((predictedY[i] >= cutOff) ? 1 : 0);
-            if ((Y[i] == 1) && (predY == 1)) TP += 1;
-            else if ((Y[i] == 0) && (predY == 1)) FP += 1;
-            else if ((Y[i] == 0) && (predY == 0)) TN += 1;
-            else if ((Y[i] == 1) && (predY == 0)) FN += 1;
+            int predY = ((predictedY[i] >= threshold) ? 1 : 0);
+
+            if ((Y[i] == 1) && (predY == 1)) TP++;
+            else if ((Y[i] == 0) && (predY == 1)) FP++;
+            else if ((Y[i] == 0) && (predY == 0)) TN++;
+            else if ((Y[i] == 1) && (predY == 0)) FN++;
         }
 
-        FNR = 1.0 * FN / (TP + FN);
-        FPR = 1.0 * FP / (TN + FP);
+        precision = 1.0 * TP / (TP + FP);
+        recall = 1.0 * TP / (TP + FN);
+        accuracy = 1.0 * (TP + TN) / (TP + TN + FP + FN);
 
         DecimalFormat df = new DecimalFormat("##.###");
         df.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.US));
 
         System.out.println();
-        System.out.println("Error rates (cutOff=" + cutOff + "):");
-        System.out.println("False Negative Rate= " + df.format(FNR));
-        System.out.println("False Positive Rate= " + df.format(FPR));
+        System.out.println("Performance report:");
         System.out.println("TP= " + TP + " FP= " + FP + " TN= " + TN + " FN= " + FN);
+        System.out.println("Precision= " + df.format(precision));
+        System.out.println("Recall= " + df.format(recall));
+        System.out.println("Accuracy= " + df.format(accuracy));
         System.out.println();
-
     }
 
-    private double arrayMax(double[] dataArray) {
-        double maxVal = 0.0;
-        for (double v : dataArray) {
-            if (v > maxVal) maxVal = v;
+    public void printModel() {
+        DecimalFormat df = new DecimalFormat("###.####");
+        df.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.US));
+        System.out.println("Sequential logistic regression weights:");
+        for (double weight : weights) {
+            System.out.print(df.format(weight) + " ");
         }
-        return maxVal;
+        System.out.println();
     }
 
-    private double arrayMin(double[] dataArray) {
-        double minVal = Double.MAX_VALUE;
-        for (double v : dataArray) {
-            if (v < minVal) minVal = v;
-        }
-        return minVal;
-    }
-
-    /**
-     * Simple standardization
-     */
-    public void standardize(double[][] dataArray) {
-        double[] varX = new double[dataArray.length];
-        for (int i = 0; i < dataArray[0].length; i++) {
-            for (int j = 0; j < dataArray.length; j++) {
-                varX[j] = dataArray[j][i];
-            }
-            double maxVal = arrayMax(varX);
-            double minVal = arrayMin(varX);
-            if (maxVal == minVal)
-                continue;
-            for (int j = 0; j < dataArray.length; j++) {
-                dataArray[j][i] = (dataArray[j][i] - minVal) / (maxVal - minVal);
-            }
-        }
+    public double[] getWeights() {
+        return weights;
     }
 }
