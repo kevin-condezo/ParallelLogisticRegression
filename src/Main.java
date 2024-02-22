f484759f5ab7ee33cca2989abe7413f18d9d1abe
Kevin Condezo <86019347+KevinC2V@users.noreply.github.com>
2024-02-22 11:46:29 -0500

    update cli

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 import LogisticRegression.*;
 import Utils.DataSet;
 
+import java.util.Scanner;
+
 public class Main {
+    static double learningRate = 0.005;
+    static int numIterations = 500;
+    static double threshold = 0.5; // Threshold for classification
+    static double testSize = 0.2; // Portion of the test subset
+
+    static DataSet ds;
+    static double[][] XTrain;
+    static int[] YTrain;
+    static double[][] XTest;
+    static int[] YTest;
+
+    static final int NUM_EVAL_RUNS = 3;
+    static Scanner in;
+
     public static void main(String[] args) {
 
         if (args.length != 1) {
@@ -10,45 +26,134 @@ public class Main {
         }
         String inputFile = args[0];
 
-        // Read csv file
-        System.out.print("Loading data...");
-        DataSet inputData = new DataSet();
-        inputData.readDataSet(inputFile);
-        System.out.println(" DONE");
+        System.out.println("\nJOKER PROJECT: LOGISTIC REGRESSION\n");
 
-        // Hyper parameters
-        double learningRate = 0.005;
-        int numIterations = 500;
-        double threshold = 0.5; // Threshold for classification
-        double testSize = 0.2; // Portion of the test set
+        // Load dataset
+        loadDataset(inputFile);
+
+        // Using Scanner for Getting Input from User
+        in = new Scanner(System.in);
+
+        System.out.print("\nPress Enter to continue...");
+        in.nextLine();
+
+        // MENU
+        char opt;
+
+        do {
+            System.out.println("\n\nChoose an option:\n");
+            System.out.println("1. Train model with Sequential Logistic Regression");
+            System.out.println("2. Train model with Parallel Logistic Regression");
+            System.out.println("3. Evaluate Parallel Performance");
+            System.out.println("4. Find best dataset size for parallelism");
+            System.out.println("5. Set hyper-parameters");
+            System.out.println("q. Quit");
+
+            System.out.print("> ");
+            String line = in.nextLine();
+            opt = (!line.isEmpty()) ? line.charAt(0) : ' ';
+
+            switch (opt) {
+                case '1':
+                    trainWithSequentialVersion();
+                    break;
+                case '2':
+                    trainWithParallelVersion();
+                    break;
+                case '3':
+                    evaluateParallelPerformance();
+                    break;
+                case '4':
+                    findOptimalDatasetSizeForParallelism();
+                    break;
+                case '5':
+                    setHyperparameters();
+                    break;
+                case 'q':
+                case 'Q':
+                    break;
+                default:
+                    System.out.println("\nInvalid option");
+            }
+            System.out.print("\nPress Enter to continue...");
+            in.nextLine();
+        } while (opt != 'q' && opt != 'Q');
+    }
+
+    static void loadDataset(String inputFile) {
+        // Read csv file
+        System.out.print("Loading dataset...");
+        ds = new DataSet();
+        ds.readDataSet(inputFile);
+        System.out.println(" DONE");
 
         // Scale predictor variables
         System.out.print("Scaling data...");
-        inputData.normalize();
+        ds.normalize();
         System.out.println(" DONE");
 
         // Split data into training and test sets
         System.out.print("Splitting data into training and test sets...");
-        inputData.splitData(testSize);
+        ds.splitData(testSize);
         System.out.println(" DONE");
 
+        // Print data set information
+        ds.printDataSetInfo();
+
         // Variables
-        double[][] XTrain = inputData.getXTrain();
-        int[] YTrain = inputData.getYTrain();
-        double[][] XTest = inputData.getXTest();
-        int[] YTest = inputData.getYTest();
+        XTrain = ds.getXTrain();
+        YTrain = ds.getYTrain();
+        XTest = ds.getXTest();
+        YTest = ds.getYTest();
+    }
 
+    static void trainWithSequentialVersion() {
+        // Create instance of the sequential logistic regression
+        SequentialLogisticRegression seqLogistic = new SequentialLogisticRegression(
+                XTrain[0].length, learningRate, numIterations, threshold
+        );
 
-        final int NUM_EVAL_RUNS = 3;
+        // Train model
+        System.out.println("\nTraining model with Batch Gradient Descent");
+        seqLogistic.trainModelWithBGD(XTrain, YTrain);
+        System.out.println("Training DONE\n");
 
-        System.out.println("Evaluating Sequential Implementation...");
+        // Print model weights
+        seqLogistic.printModel();
+
+        // Compute errors
+        double[] predictedY = seqLogistic.scoreData(XTest);
+        seqLogistic.evaluateModel(YTest, predictedY);
+    }
+
+    static void trainWithParallelVersion() {
+        // Create instance of the parallel logistic regression
+        ParallelLogisticRegression parLogistic = new ParallelLogisticRegression(
+                XTrain[0].length, learningRate, numIterations, threshold
+        );
+
+        // Train model
+        System.out.println("\nTraining model with Batch Gradient Descent");
+        parLogistic.trainModelWithBGD(XTrain, YTrain);
+        System.out.println("Training DONE\n");
+
+        // Print model weights
+        parLogistic.printModel();
+
+        // Compute errors
+        double[] predictedY = parLogistic.scoreData(XTest);
+        parLogistic.evaluateModel(YTest, predictedY);
+    }
+
+    static void evaluateParallelPerformance() {
+        System.out.println("\nEvaluating Sequential Implementation...");
         // Create instance of the sequential logistic regression
         SequentialLogisticRegression seqLogistic = new SequentialLogisticRegression(
                 XTrain[0].length, learningRate, numIterations, threshold
         );
 
         double sequentialTime = 0;
-        for(int i=0; i<NUM_EVAL_RUNS; i++) {
+        for (int i = 0; i < NUM_EVAL_RUNS; i++) {
             long start = System.currentTimeMillis();
             // Train model
             System.out.println("\nTraining model with Batch Gradient Descent");
@@ -58,14 +163,15 @@ public class Main {
         }
         sequentialTime /= NUM_EVAL_RUNS;
 
-        System.out.println("Evaluating Parallel Implementation...");
+
+        System.out.println("\nEvaluating Parallel Implementation...");
         // Create instance of the sequential logistic regression
         ParallelLogisticRegression parLogistic = new ParallelLogisticRegression(
                 XTrain[0].length, learningRate, numIterations, threshold
         );
 
         double parallelTime = 0;
-        for(int i=0; i<NUM_EVAL_RUNS; i++) {
+        for (int i = 0; i < NUM_EVAL_RUNS; i++) {
             long start = System.currentTimeMillis();
             // Train model
             System.out.println("\nTraining model with Batch Gradient Descent");
@@ -83,10 +189,85 @@ public class Main {
         double[] predictedY = parLogistic.scoreData(XTest);
         parLogistic.evaluateModel(YTest, predictedY);
 
+        double speedup = sequentialTime / parallelTime;
+        int numProcessors = Runtime.getRuntime().availableProcessors();
+        double efficiency = 100 * speedup / numProcessors;
+
         System.out.println();
+        System.out.println("Performance Evaluation: ");
         System.out.format("Average Sequential Time: %.1f ms\n", sequentialTime);
         System.out.format("Average Parallel Time: %.1f ms\n", parallelTime);
-        System.out.format("Speedup: %.2f \n", sequentialTime/parallelTime);
-        System.out.format("Efficiency: %.2f%%\n", 100*(sequentialTime/parallelTime)/Runtime.getRuntime().availableProcessors());
+        System.out.format("Speedup: %.2f \n", speedup);
+        System.out.println("Number of processors: " + numProcessors);
+        System.out.format("Efficiency: %.2f%%\n", efficiency);
+    }
+
+    static void setHyperparameters() {
+        System.out.println("\n\nEnter hyperparameters: ");
+        System.out.print("learning rate (" + learningRate + "): ");
+        learningRate = in.nextDouble();
+        System.out.print("number of iterations (" + numIterations + "): ");
+        numIterations = in.nextInt();
+        System.out.print("threshold (" + threshold + "): ");
+        threshold = in.nextDouble();
+    }
+
+    static void findOptimalDatasetSizeForParallelism() {
+        int nFeatures = 10;
+        int nIterations = 90;
+        int minObservations = 500;
+        int maxObservations = 100_000;
+        int numRuns = 10;
+        double speedup = 0;
+
+        for (int n = minObservations; n <= maxObservations; n += 500) {
+            System.out.println("\n\nEvaluating dataset with " + n + " observations...");
+            DataSet myDs = new DataSet();
+            myDs.generateDataset(n, nFeatures);
+            double[][] X = myDs.getX();
+            int[] Y = myDs.getY();
+
+            // Create instance of the sequential logistic regression
+            SequentialLogisticRegression seqLogistic = new SequentialLogisticRegression(
+                    nFeatures, learningRate, nIterations, threshold
+            );
+
+            double sequentialTime = 0;
+            for (int i = 0; i < numRuns; i++) {
+                long start = System.currentTimeMillis();
+                seqLogistic.trainModelWithBGD(X, Y);
+                sequentialTime += System.currentTimeMillis() - start;
+            }
+            sequentialTime /= numRuns;
+
+            // Create instance of the sequential logistic regression
+            ParallelLogisticRegression parLogistic = new ParallelLogisticRegression(
+                    nFeatures, learningRate, nIterations, threshold
+            );
+
+            double parallelTime = 0;
+            for (int i = 0; i < numRuns; i++) {
+                long start = System.currentTimeMillis();
+                parLogistic.trainModelWithBGD(X, Y);
+                parallelTime += System.currentTimeMillis() - start;
+            }
+            parallelTime /= numRuns;
+
+            speedup = sequentialTime / parallelTime;
+            int numProcessors = Runtime.getRuntime().availableProcessors();
+            double efficiency = 100 * speedup / numProcessors;
+
+            System.out.println();
+            System.out.println("Performance Evaluation: ");
+            System.out.format("Average Sequential Time: %.1f ms\n", sequentialTime);
+            System.out.format("Average Parallel Time: %.1f ms\n", parallelTime);
+            System.out.format("Speedup: %.2f \n", speedup);
+            System.out.format("Efficiency: %.2f%%\n", efficiency);
+
+            if (speedup > 1.0) {
+                System.out.println("\nApproximate optimal dataset size for parallelism: " + n);
+                break;
+            }
+        }
     }
 }
